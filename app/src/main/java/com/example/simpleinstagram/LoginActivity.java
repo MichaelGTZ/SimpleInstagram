c58dd419b22a54444d8ed55480bf059d7f9e61b5
Michael Gutierrez <michaelgtz@fb.com>
2020-07-07 15:53:39 -0600

    Complete through video Instagram Ep 4 - Parse Login and Navigation Creates login functionality and brings users to an empty MainActivity The Instagram icon shows up on the home screen of the device.

---
+++ app/src/main/java/com/example/simpleinstagram/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.example.simpleinstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // If someone is logged in already, don't show login screen and go directly to home screen
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "loginUser: Attempting to login user: " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() { // Log in in background on a secondary thread
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    // TODO: Better error handling to let user know the problem
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Issue with Login", Toast.LENGTH_SHORT);
+                    return;
+                } else {
+                    goMainActivity();
+                    Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish(); // This prevents the user from being able to press the back button to go back to login screen after logging in
+    }
+}
